d8747e05fde26c08c1d49fe6d292ddb496e3820d
Oscar Hilton <oscar.hilton@mobile-5.com>
2018-09-18 17:27:28 +0100

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,234 @@
+import React from "react";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { WebBrowser, Audio, Permissions, FileSystem } from "expo";
+
+import { MonoText } from "../components/StyledText";
+
+export default class HomeScreen extends React.Component {
+  constructor() {
+    super();
+    this.sound = null;
+    this.recording = null;
+    this.state = {
+      haveRecordingPermissions: null,
+      loading: false,
+      recording: false,
+    };
+    this.recordingSettings = JSON.parse(
+      JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY)
+    );
+  }
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  componentDidMount() {
+    this._askForPermissions();
+  }
+
+  _askForPermissions = async () => {
+    const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    this.setState({
+      haveRecordingPermissions: response.status === "granted",
+    });
+  };
+
+  async _beginRecording() {
+    this.setState({
+      loading: true,
+      recording: true,
+    });
+    if (this.sound !== null) {
+      await this.sound.unloadAsync();
+      this.sound.setOnPlaybackStatusUpdate(null);
+      this.sound = null;
+    }
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+    });
+    if (this.recording !== null) {
+      this.recording.setOnRecordingStatusUpdate(null);
+      this.recording = null;
+    }
+
+    const recording = new Audio.Recording();
+    await recording.prepareToRecordAsync(this.recordingSettings);
+    console.log("recording!!");
+
+    this.recording = recording;
+    await this.recording.startAsync();
+    this.setState({
+      loading: false,
+      recording: this.recording !== null,
+    });
+  }
+
+  async _endRecording() {
+    console.log("end recording");
+    this.setState({
+      loading: true,
+    });
+    try {
+      await this.recording.stopAndUnloadAsync();
+    } catch (e) {
+      throw e;
+    }
+    const info = await FileSystem.getInfoAsync(this.recording.getURI());
+    console.log(`FILE INFO: ${JSON.stringify(info)}`);
+    console.log(info.uri);
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      playsInSilentLockedModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+    });
+    const { sound } = await this.recording.createNewLoadedSound({
+      isLooping: true,
+      isMuted: false,
+      volume: 1.0,
+      rate: 1.0,
+      shouldCorrectPitch: false,
+    });
+    this.sound = sound;
+    this.sound.playAsync();
+    this.setState({
+      loading: false,
+    });
+  }
+
+  render() {
+    const { recording } = this.state;
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.getStartedContainer}>
+            {!recording ? (
+              <Text
+                style={styles.getStartedText}
+                onPress={this._beginRecording.bind(this)}
+              >
+                Start recording
+              </Text>
+            ) : (
+              <Text
+                style={styles.getStartedText}
+                onPress={this._endRecording.bind(this)}
+              >
+                Stop recording
+              </Text>
+            )}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center",
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: "rgba(96,100,109, 0.8)",
+  },
+  codeHighlightContainer: {
+    backgroundColor: "rgba(0,0,0,0.05)",
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center",
+  },
+  tabBarInfoContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center",
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: "center",
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: "#2e78b7",
+  },
+});
